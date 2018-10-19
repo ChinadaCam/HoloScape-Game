98fd4bfdfcfa57420d6d748cd57b0ee0c699e80d
Faustino <sogamespt@gmail.com>
2018-10-19 11:41:41 +0100

    V. 0.0.5
    Game:
     Fix camera controls.
     Increased Player movement.
    Main Menu:
     +Help Button.
    Options Menu:
     +Resolutions.
     +Fullscreen.
     +Volume.

--- Hologram/Assets/Scripts/CameraControl.cs
+++ Hologram/Assets/Scripts/CameraControl.cs
@@ -7,14 +7,14 @@ public class CameraControl : MonoBehaviour {
     protected Transform _XForm_Parent;
 
     protected Vector3 _LocalRotation;
-    protected float _CameraDistance = 10f;
+    protected float _CameraDistance = 15f;
 
     public float MouseSensitivity = 4f;
     public float ScrollSensitvity = 2f;
     public float OrbitDampening = 10f;
     public float ScrollDampening = 6f;
-
-    public bool CameraDisabled = false;
+  
+    public bool CameraDisabled = true;
 
 
     // Use this for initialization
@@ -24,15 +24,22 @@ public class CameraControl : MonoBehaviour {
         this._XForm_Parent = this.transform.parent;
     }
 
+    void Update()
+    {
+      
+    }
+
 
     void LateUpdate()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift))
             CameraDisabled = !CameraDisabled;
 
-        if (!CameraDisabled)
+        if(Input.GetMouseButton(1) == true) { 
+        if (!CameraDisabled  )
         {
             //Rotation of the Camera based on Mouse Coordinates
+            
             if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
             {
                 _LocalRotation.x += Input.GetAxis("Mouse X") * MouseSensitivity;
@@ -44,8 +51,12 @@ public class CameraControl : MonoBehaviour {
                 else if (_LocalRotation.y > 90f)
                     _LocalRotation.y = 90f;
             }
-            //Zooming Input from our Mouse Scroll Wheel
-            if (Input.GetAxis("Mouse ScrollWheel") != 0f)
+
+            }
+        }
+
+        //Zooming Input from our Mouse Scroll Wheel
+        if (Input.GetAxis("Mouse ScrollWheel") != 0f)
             {
                 float ScrollAmount = Input.GetAxis("Mouse ScrollWheel") * ScrollSensitvity;
 
@@ -55,10 +66,9 @@ public class CameraControl : MonoBehaviour {
 
                 this._CameraDistance = Mathf.Clamp(this._CameraDistance, 1.5f, 100f);
             }
-        }
 
-        //Actual Camera Rig Transformations
-        Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
+            //Actual Camera Rig Transformations
+            Quaternion QT = Quaternion.Euler(_LocalRotation.y, _LocalRotation.x, 0);
         this._XForm_Parent.rotation = Quaternion.Lerp(this._XForm_Parent.rotation, QT, Time.deltaTime * OrbitDampening);
 
         if (this._XForm_Camera.localPosition.z != this._CameraDistance * -1f)
