b4ad1ff78640bb114c27a16035c90f4750810478
Faustino <sogamespt@gmail.com>
2018-10-20 17:48:36 +0100

    V. 0.1.0
    Game:
     +FPS Display(F9)
     Tweaked movement.
    Menu:
     Changed font to "6869 chargen".

---
+++ Hologram/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour {
+
+    public LoadSceneMode load;
+
+    public void Select(string levelName)
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Hologram/Assets/Scripts/SceneFade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneFade : MonoBehaviour {
+
+    public Image img;
+    public AnimationCurve curve;
+
+
+    void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        float time = 1f;
+        //Animate fade
+        while (time > 0f)
+        {
+            float a = curve.Evaluate(time);
+            img.color = new Color(0f, 0f, 0f, time);
+
+            time -= Time.deltaTime*2f;
+            yield return 0;
+        }
+        //Load scene
+    }
+
+
+}
